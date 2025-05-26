e5e6838628050ba0f57e573c494ebf15d8233a0f
maripachon <pachonmaritza102@gmail.com>
2025-05-26 08:50:36 -0500

    Update Moto.java

--- src/Moto.java
+++ src/Moto.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+public class Moto extends Vehiculo implements VehiculoConPlaca {
+    private String placa;
 
-public class Moto {
+    public Moto(Persona persona, String color, String placa) {
+        super(persona, color);
+        this.placa = placa;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
 }
