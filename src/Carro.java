707ee7af8e9883e0d77f146c6791983140755317
maripachon <pachonmaritza102@gmail.com>
2025-05-26 08:49:06 -0500

    Update Carro.java

--- src/Carro.java
+++ src/Carro.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+public class Carro extends Vehiculo implements VehiculoConPlaca {
+    private String placa;
 
-public class Carro {
+    public Carro(Persona persona, String color, String placa) {
+        super(persona, color);
+        this.placa = placa;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
 }
