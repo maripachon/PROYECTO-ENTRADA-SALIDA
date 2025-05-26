2e2cae6bc34a9932b4dd7bb3f5a39dc60700339c
maripachon <pachonmaritza102@gmail.com>
2025-05-26 08:51:23 -0500

    Update Persona.java

--- src/Persona.java
+++ src/Persona.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Persona {
+    private String nombre;
+    private String id;
+
+    public Persona(String nombre, String id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
