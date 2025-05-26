fb0c587fe03358c9ecf0162e4d9c9ee39607e3bb
maripachon <pachonmaritza102@gmail.com>
2025-05-26 08:49:40 -0500

    Update Computador.java

--- src/Computador.java
+++ src/Computador.java
@@ -1,2 +1,42 @@
-public class RegistrodeComputadores {
+import java.time.LocalDateTime;
+
+public class Computador {
+    private Persona persona;
+    private String marca;
+    private String serial;
+    private LocalDateTime horaIngreso;
+    private LocalDateTime horaSalida;
+
+    public Computador(Persona persona, String marca, String serial) {
+        this.persona = persona;
+        this.marca = marca;
+        this.serial = serial;
+        this.horaIngreso = LocalDateTime.now();
+        this.horaSalida = null;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public LocalDateTime getHoraIngreso() {
+        return horaIngreso;
+    }
+
+    public LocalDateTime getHoraSalida() {
+        return horaSalida;
+    }
+
+    public void registrarSalida() {
+        this.horaSalida = LocalDateTime.now();
+    }
 }
+
